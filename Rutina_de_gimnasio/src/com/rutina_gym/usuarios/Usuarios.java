b7a9f19330053728fc0504e524c731034da1d92e
Rodrigodls10 <rodrigodls.1997@gmail.com>
2024-05-26 18:33:26 -0300

    Usuarios

---
+++ Rutina_de_gimnasio/src/com/rutina_gym/usuarios/Usuarios.java
@@ -0,0 +1,22 @@
+package com.rutina_gym.usuarios;
+
+public class Usuarios {
+
+
+            private String nombre;
+            private String IdUsuario;
+
+            public Usuarios (String nombre, String IdUsuario) {
+                this.nombre = nombre;
+                this.IdUsuario = IdUsuario;
+            }
+
+            public String getNombre() {
+                return nombre;
+            }
+
+            public String getIdUsuario() {
+                return IdUsuario;
+            }
+    }
+
